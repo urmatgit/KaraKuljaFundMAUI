87b48b4512466008926b6777d6850fdd5a482b0b
urmatgit <urmat2g@gmail.com>
2023-12-06 16:07:49 +0300

    Add MAUI project

---
+++ KaraKuljaFund/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KaraKuljaFund
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
